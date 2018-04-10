ea5726c078227af63086c28d288d41304a674bea
Emma <emma@goding.eu>
2018-04-10 14:33:35 +0200

    clean up and comments

--- 01_Trim/trimTails_noAdapt.R
+++ 01_Trim/trimTails_noAdapt.R
@@ -1,20 +1,29 @@
+
+## Trimming sequences using trimTails which uses sliding window 2*halfwith+1
+## If a window contains k bases with phred score <= a the sequence is cut 
+## at the start of the window
+## Uses the dataset where primers have already been removed
+## One of two method used for trimming the data 
+
 #path <-  file.path("C:", "Users", "Karamech", "Documents", "Utbildning", "Kandidatarbete", "Data")
 path <-  file.path("..", "00_Data") #Change to root of filtered Data
-NoAdapPath <-  file.path(path, "AdaptersRemoved")
-TrimPath <- file.path(path, "trimmedTailsw")
-dir.create(TrimPath)
+NoAdapPath <-  file.path(path, "AdaptersRemoved") #Set to directory whith files to trim
+TrimPath <- file.path(path, "trimmedTailsw") #new directory with trimmed sequences, created in root for data
 
 library(ShortRead); packageVersion("ShortRead")
 
+dir.create(TrimPath) #Creating directory for new files
+
+##Finding all files and creating paths to new files with the same names
 R1Files <- file.path(NoAdapPath, list.files(NoAdapPath, pattern="_R1_001.fastq.gz"))
 R2Files <- file.path(NoAdapPath, list.files(NoAdapPath, pattern="_R2_001.fastq.gz"))
 
 sample.names <- sapply(strsplit(basename(R1Files), "_"), `[`, 1)
-#filt_path <- file.path(path, "filtered") # Path for placing filtered files
-R1TrimPath <- file.path(TrimPath, paste0(sample.names, "_R1_trimTails.fastq.gz"))
-R2TrimPath <- file.path(TrimPath, paste0(sample.names, "_R2_trimTails.fastq.gz"))
+R1TrimPath <- file.path(TrimPath, paste0(sample.names, "_R1_001.fastq.gz"))
+R2TrimPath <- file.path(TrimPath, paste0(sample.names, "_R2_001.fastq.gz"))
 
-## Filtering files using trimTailsw
+## Filtering files using trimTailsw.
+## Same quality check for R1 and R2
 trimWhen <- character(length=1)
 trimWhen[1] <- "+" # ASCII character, trimTailsw trims at first instance from the left of base with phred score <= trimWhen"
 nbrFail <- integer(1)
