eae81f12bd903c4084dac1c0f1807e460591d556
Emmah <emmahkansson@gmail.com>
2018-04-18 08:27:07 +0200

    saving taxa and taxa.print as RDS files

--- 03_Assign/assign_taxa.R
+++ 03_Assign/assign_taxa.R
@@ -1,14 +1,15 @@
-
 path <-  file.path("..", "00_Data") #Path to root of all data
-TnFpath <- file.path(path, "clean_and_matched_1") #Path to directory of sequences that have been trimmed and filtered
+TnFpath <- file.path(path, "clean_and_matched_2") #Path to directory of sequences that have been trimmed and filtered
 
 library(dada2); packageVersion("dada2")
 
 nochim <- readRDS(file.path(TnFpath, "nochim.rds"))
 
-taxa <- assignTaxonomy(nochim, file.path(path, "silva_nr_v132_train_set.fa.gz", multithread=TRUE)
+taxa <- assignTaxonomy(nochim, file.path(path, "silva_nr_v132_train_set.fa.gz"), multithread=TRUE)
 
 taxa.print <- taxa # Removing sequence rownames for display only
 rownames(taxa.print) <- NULL
 print(taxa.print)
 
+saveRDS(taxa, file.path(TnFpath, "taxa.rds"))
+saveRDS(taxa.print, file.path(TnFpath, "taxa_print.rds"))
