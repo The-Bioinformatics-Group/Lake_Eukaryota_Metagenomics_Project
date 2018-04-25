4192868c9f0676eec88c4dede248ef3f99dd6d5f
Emma <emma@goding.eu>
2018-04-25 08:16:11 +0200

    updated files for transfering data to phyloseq, also reads metadata

--- 04_Analysis/to_phyloseq_testing.R
+++ 04_Analysis/to_phyloseq_testing.R
@@ -2,7 +2,7 @@
 ## Extracts the sequence table and taxa table as RDS files from the specified directory
 
 path <-  file.path("..", "00_Data") 				#Path to root of all data
-TnFpath <- file.path(path, "clean_and_matched_2") 	#Path to directory containing RDS files
+TnFpath <- file.path(path, "clean_and_matched_2_old") 	#Path to directory containing RDS files
 
 ##load packages
 library(phyloseq); packageVersion("phyloseq")
@@ -19,15 +19,15 @@ samdf <- data.frame(Subject=subject)
 rownames(samdf) <- samples.out
 
 ##Creating and saving phyloseq object
-psS <- phyloseq(otu_table(nochim, taxa_are_rows=FALSE), sample_data(samdf), tax_table(taxa))
+ps <- phyloseq(otu_table(nochim, taxa_are_rows=FALSE), sample_data(samdf), tax_table(taxa))
 #saveRDS(ps, file.path(TnFpath, "ps.rds"))
 
-metafile <- read.csv(file.path(path, "100lakes_metadata_ed_1.csv"), sep=";")
-metadata <- array(metafile, dimnames=list(sample_names(psS), colnames(metafile)))
+metafile <- read.csv(file.path(path, "100lakes_metadata_ed_short.csv"), sep=";")
+metadata <- array(metafile, dimnames=list(sample_names(ps), colnames(metafile)))
 
 vardata = sample_data(data.frame(Sample=metadata["sampleID"], Latitude=metadata["Latitude"], pH=metadata["pH"]))
-psS1 <- merge_phyloseq(psS, vardata)
+ps1 <- merge_phyloseq(ps, vardata)
 
-psS1.na = subset_taxa(psS1, is.na(Family))
+psS1.na = subset_taxa(ps1, is.na(Family))
 #tax_table(psS1.na)[1:10, 1:6]
-psS1.notna = subset_taxa(psS1, !is.na(Family))
+psS1.notna = subset_taxa(ps1, !is.na(Family))
